4d64ef9c9d8e39e399a1e8674898c2705100b5f9
sudhanshu2070 <sssallen2016@gmail.com>
2025-02-09 01:18:51 +0530

    Theme selector 0.7

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
-// App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/contexts/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 import LoadingScreen from './src/screens/LoadingScreen';
 import { useFontLoader } from './src/utils/fontLoader';
-import { Provider } from 'react-redux';
-import store from './src/store/store';
+import { Provider, useSelector } from 'react-redux';
+import store, { RootState } from './src/store/store';
 import ThemeProvider from './src/components/ThemeProvider'; 
+import { StatusBar } from 'react-native';
+import { themes } from './src/utils/themes';
 
 const App = () => {
   const fontsLoaded = useFontLoader();
+  // const theme = useSelector((state: RootState) => state.theme.theme);
+
   if (!fontsLoaded) {
     return <LoadingScreen />;
   }
