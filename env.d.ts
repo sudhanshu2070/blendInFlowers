8470ac43b2523ccc8dc1d4d9cd15e490b41abd91
sudhanshu2070 <sssallen2016@gmail.com>
2025-03-08 23:55:19 +0530

    Added Weather Icon in YourSpace 0.41

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,14 @@
+/**
+ * Module declaration for '@env'.
+ * 
+ * This module is used to provide type definitions for environment variables
+ * that are imported from the '@env' module. In this case, it declares a single
+ * environment variable `OPEN_WEATHER_API_KEY` which is a string.
+ * 
+ * Having this declaration helps TypeScript understand the types of the environment
+ * variables being used in the project, providing better type safety and autocompletion
+ * support in the development environment.
+ */
 declare module '@env' {
     export const OPEN_WEATHER_API_KEY: string;
   }
